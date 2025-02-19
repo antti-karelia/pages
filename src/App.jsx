4286d77a8b5ea8dd6d40e196befaf4acb53120ac
Antti Hurme <antti.hurme@karelia.fi>
2025-02-19 10:18:57 +0200

    initial

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ const App = ({ notes }) => {
   return (
     <div>
       <h1>Notes</h1>
+      <h2>Hello Ossi</h2>
       <ul>
         {notes.map(note => 
           <Note key={note.id} note={note} />
